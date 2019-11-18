9357da5c99f3fef1d02a945c4be34c10ab499eb3
Philipp Proff <philipp.proff@gmx.de>
2019-11-18 10:35:46 +0100

    Site switch bug

--- Dynamic/points.js
+++ Dynamic/points.js
@@ -4,8 +4,8 @@ const show_points = (cluster,spec,part) => {//cluster bsp:ring spec bsp:Build-Up
   .style("display","none")
   //Create the HTML elements if they are missing
   var points_length = d3.selectAll("#points > div").nodes().length
-  if(points_length < (60/5)){
-    var diff = (60/5) - points_length
+  if(points_length < (65/5)){
+    var diff = (65/5) - points_length
     for(let i = 0; i < diff;i++){
       let div = d3.select("#points").append("div")
       for(let y = 0; y < 5;y++){
@@ -13,6 +13,8 @@ const show_points = (cluster,spec,part) => {//cluster bsp:ring spec bsp:Build-Up
       }
     }
   }
+  //Passing the array to the show function ->  could be in one function in future, but need a seperate
+  //function for showing the search results later 
   var array = points.filter(p => p[cluster] == spec && (cluster == "ring"?p.topic:p.ring) == part)
   show_points_array(cluster,spec,part,array,0)
 }
@@ -28,13 +30,13 @@ const show_points_array = (cluster,spec,part,array,page) => {
       return `${spec} / ${part} /${array_length} Items`
     }
   })
-
-  array.slice((page*40),((page+1)*40)).map((point,pointIndex) => {
-    d3.selectAll("#points > div:not(.parts_holder) > p").filter((d,i) => (i == pointIndex)).html(point.name)
-    .on("mouseover",() => {single_highlight(point.id)})
-    .on("mouseout",() => {delight()})
-    .on("click",() => {show_point(point)})
-  })
+  var sliced = array.length > 45? array.slice((page*45),((page+1)*45)):array.slice(0,45)
+    sliced.map((point,pointIndex) => {
+      d3.selectAll("#points > div:not(.parts_holder) > p").filter((d,i) => (i == pointIndex)).html(point.name)
+      .on("mouseover",() => {single_highlight(point.id)})
+      .on("mouseout",() => {delight()})
+      .on("click",() => {show_point(point)})
+    })
   var parts = [...new Set(points.filter(p => p[cluster] == spec).map(p => p = (cluster == "ring"?p.topic:p.ring)))]
   var parts_holder_length = d3.select(".parts_holder > p").nodes().length
   if(parts.length > parts_holder_length){
@@ -49,14 +51,14 @@ const show_points_array = (cluster,spec,part,array,page) => {
     .on("click",(d,i) => {show_points(cluster,spec,parts)})
   })
 
-  if(array_length > ((page+1)*40)){
+  if(array_length > ((page+1)*45)){
     d3.select("#points > .pages > #pages-arrow-right").style("display","block")
     .on("click",() => {show_points_array(cluster,spec,part,array,(page+1))})
   }else{
     d3.select("#points > .pages > #pages-arrow-right")
     .style("display","none")
   }
-  if(((page+1)*40) != 40){
+  if(((page+1)*45) != 45){
     d3.select("#points > .pages > #pages-arrow-left").style("display","block")
     .on("click",() => {show_points_array(cluster,spec,part,array,(page-1))})
   }else{
