6d9bdd4cfb7dc33c48ae3ed7bc44b4362b0a4916
elproffesore <philipp.proff@gmx.de>
2019-12-11 14:10:13 +0100

    RegEx for Description

--- Dynamic/point.js
+++ Dynamic/point.js
@@ -20,7 +20,37 @@ var show_point = (point) => {
   d3.selectAll(".point_information >.info> p").filter((d, i) => (i == 1)).html("Topic: " + (point.topic || "No Topic assigned"));
   d3.selectAll(".point_information >.info> p").filter((d, i) => (i == 2)).html("Strategic Topic: " + (point.subtopic || "No Strategic Topic assigned"));
   d3.selectAll(".point_information >.info> p").filter((d, i) => (i == 3)).html("Category: " + point.category);
-  d3.selectAll(".point_information >.description> p").filter((d, i) => (i == 0)).html(point.description);
-  d3.select(".novatec_information > article").html(point.whatwedo || "No description available")
+
+
+
+  d3.selectAll(".point_information >.description> p").filter((d, i) => (i == 0))
+      .html(() => {
+            if(point.description !== null){
+                let string = point.description.replace(/\^?\[(.*?)\|(.*?)]\^?/g,function(match,p1,p2){
+                    if(p1.includes("[")){
+                        p1 = "<sup>"+p1+"</sup>"
+                    }
+                    return "<a href='"+p2+"' target='_blank'>"+p1+"</a>"
+                });
+                string = string.replace(/\[(http[^\s]*)]/g,function(match,p1){
+                    return "<p><a href='"+p1+"' target='_blank'>"+p1+"</a></p>"
+                });
+                string = string.replace(/(([\s]|^)http[^\s]*)/g,function(match,p1){
+                    return "<p><a href='"+p1+"' target='_blank'>"+p1+"</a></p>"
+                });
+                string = string.replace(/\*(.[^*]*)\*/g,function(match,p1){
+                    return "<strong>"+p1+"</strong>"
+                });
+                string = string.replace(/h1\.(.[^\n]*)/g,function(match,p1){
+                   return "<h2>"+p1+"</h2>"
+                });
+                string = string.replace(/h2\.(.[^\n]*)/g,function(match,p1){
+                    return "<h3>"+p1+"</h3>"
+                });
+                return string
+            }
+      });
+  d3.select(".novatec_information > article")
+      .html(point.whatwedo)
 
 };
